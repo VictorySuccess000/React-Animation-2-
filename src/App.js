8e6f794e4e47cce57953e2cbc5a0b344a6edd023
best-lucky1030 <bestlucky1030@gmail.com>
2021-12-12 05:54:41 +0900

    success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import menuIcon from "./menuIcon.jpg";
+import message from "./message.jpg";
+import pencil from "./pencil.png";
+
+const menuClick = () => {
+  document.querySelector(".menuIcon").classList.toggle("bigbtn");
+  document.querySelector(".message").classList.toggle("itemHide");
+  document.querySelector(".pencil").classList.toggle("itemHide");
+};
+
+const msgClick = () => {
+  document.querySelector(".message").classList.toggle("activemsg");
+};
+
+const pclClick = () => {
+  document.querySelector(".pencil").classList.toggle("penbgchange");
+};
+const bgchange = () => {
+  document.querySelector(".gh").classList.toggle("bgcl");
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menuIcon">
+        <div className="test" onClick={menuClick}>
+          <span className="testin"></span>
+        </div>
+        <div className="toplayout">
+          <span className="testimg" onClick={menuClick}>
+            <img className="menuImg" src={menuIcon} alt="" />
+          </span>
+          <span className="message itemHide" onClick={msgClick}>
+            <div className="msgImg gh">
+              <img className="msgImg" src={message} alt="" onClick={bgchange} />
+            </div>
+          </span>
+        </div>
+        <div className="pencil itemHide" onClick={pclClick}>
+          <img className="pclImg" src={pencil} alt="" />
+          &nbsp; Compose
+        </div>
+      </div>
     </div>
   );
 }
